16f0872990a02639c869733bfe5e4165b8cc4212
Oğuz Demirkol <74494877+ouzdmrkl@users.noreply.github.com>
2022-07-11 23:32:29 +0300

    Update Program.cs

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -4,12 +4,7 @@
     {
         public static void Main(string[] args)
         {
-            int a = 0;
-            int aas = 0;
-            int aaass = 0;
-            int aaasdsasaass = 0;
-            int aaasdassasaass = 0;
-            int aaasasaass = 0;
+            
         }
     }
 }
