d545b293c2b88cb28e40c791b1c4d49f3d1479f1
Oğuz Demirkol <ouzdmrkl@gmail.com>
2022-07-11 23:30:53 +0300

    as

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -4,7 +4,12 @@
     {
         public static void Main(string[] args)
         {
-            
+            int a = 0;
+            int aas = 0;
+            int aaass = 0;
+            int aaasdsasaass = 0;
+            int aaasdassasaass = 0;
+            int aaasasaass = 0;
         }
     }
 }
